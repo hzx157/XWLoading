a0d5567171644ca54f6764f367acf51d760eade6
Master <xiaowuxiaowu@ws-MacBook-Air.local>
2016-04-05 10:51:26 +0800

    Huangzhenxiang

--- Xw/UIAlertView+Block.h
+++ Xw/UIAlertView+Block.h
@@ -1,10 +1,6 @@
 //
 //  UIAlertView+Block.h
-//  iOS-Categories (https://github.com/shaojiankui/iOS-Categories)
-//
-//  Created by 符现超 on 15/5/9.
-//  Copyright (c) 2015年 http://weibo.com/u/1655766025 All rights reserved.
-//
+
 
 #import <UIKit/UIKit.h>
 
--- Xw/UIAlertView+Block.m
+++ Xw/UIAlertView+Block.m
@@ -1,9 +1,6 @@
 //
 //  UIAlertView+Block.m
-//  iOS-Categories (https://github.com/shaojiankui/iOS-Categories)
-//
-//  Created by 符现超 on 15/5/9.
-//  Copyright (c) 2015年 http://weibo.com/u/1655766025 All rights reserved.
+
 //
 
 #import "UIAlertView+Block.h"
